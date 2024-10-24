e9c2d1762fc856419b0b97bae0146a44ca785f52
Lincoln Souza <lincolncloud23@gmail.com>
2024-10-23 23:26:09 -0300

    chore: add shadcn setup

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,12 @@
+import { Avatar } from "./_components/ui/avatar"
+import { Button } from "./_components/ui/button"
+
 const Home = () => {
-  return <h1 className="">HELLO WORLD</h1>
+  return (
+    <h1 className="">
+      HELLO WORLD<Button>teste</Button>
+    </h1>
+  )
 }
 
 export default Home
