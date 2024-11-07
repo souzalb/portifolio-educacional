41bdb1ebdb472143d68fce6a97352ae74bbcf102
Lincoln Souza <lincolncloud23@gmail.com>
2024-11-07 10:47:14 -0300

    feat: auth domains images providers

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,11 @@
-import type { NextConfig } from "next";
-
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  images: {
+    remotePatterns: [
+      {
+        hostname: "drive.google.com",
+      },
+    ],
+  },
+}
+export default nextConfig
