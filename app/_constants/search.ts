498d09283734b69ea8203f80b4b283727c4c202d
Lincoln Souza <lincolncloud23@gmail.com>
2024-11-07 11:15:46 -0300

    feat: add search.ts component

---
+++ app/_constants/search.ts
@@ -0,0 +1,52 @@
+interface QuickSearchOption {
+  imageUrl: string
+  title: string
+}
+
+export const quickSearchOption: QuickSearchOption[] = [
+  {
+    imageUrl: "/automacao.png",
+    title: "Automação",
+  },
+  {
+    imageUrl: "/automotiva.png",
+    title: "Automotiva",
+  },
+  {
+    imageUrl: "/eletronica.png",
+    title: "Eletrônica",
+  },
+  {
+    imageUrl: "/gestao.png",
+    title: "Gestão",
+  },
+  {
+    imageUrl: "/logistica.png",
+    title: "Logística",
+  },
+
+  {
+    imageUrl: "/metalmecanica.png",
+    title: "Metalmecânica",
+  },
+  {
+    imageUrl: "/online.png",
+    title: "Aulas Online",
+  },
+  {
+    imageUrl: "/ti.png",
+    title: "TI",
+  },
+  {
+    imageUrl: "/trabalhador.png",
+    title: "Segurança do Trabalho",
+  },
+  {
+    imageUrl: "/transversal.png",
+    title: "Transversal",
+  },
+  {
+    imageUrl: "/trem.png",
+    title: "Ferroviário",
+  },
+]
