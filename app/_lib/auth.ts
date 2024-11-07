a92305de5fc6e0c24b2056128212ed04221f37b4
Lincoln Souza <lincolncloud23@gmail.com>
2024-11-07 16:35:28 -0300

    feat: add next-auth resources

---
+++ app/_lib/auth.ts
@@ -0,0 +1,28 @@
+import { PrismaAdapter } from "@auth/prisma-adapter"
+import { db } from "./prisma"
+import { Adapter } from "next-auth/adapters"
+import { AuthOptions } from "next-auth"
+import GoogleProvider from "next-auth/providers/google"
+
+export const authOptions: AuthOptions = {
+  adapter: PrismaAdapter(db) as Adapter,
+  providers: [
+    GoogleProvider({
+      clientId: process.env.GOOGLE_CLIENT_ID as string,
+      clientSecret: process.env.GOOGLE_CLIENT_SECRET as string,
+    }),
+  ],
+
+  /* eslint-disable  @typescript-eslint/no-explicit-any */
+  callbacks: {
+    async session({ session, user }) {
+      session.user = {
+        ...session.user,
+        id: user.id,
+      } as any
+      return session
+    },
+  },
+
+  secret: process.env.NEXT_AUTH_SECRET,
+}
